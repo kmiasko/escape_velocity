8e6b3148ce60b574c8c239a31700ce01342c38df
Krzysztof Miąsko <kmiasko@gmail.com>
2016-06-29 12:43:09 +0200

    added document click handler for navbar

--- js/main.js
+++ js/main.js
@@ -1,12 +1,24 @@
 (() => {
-  const hamburgerClicked = () => {
+  const hamburgerClicked = (e) => {
     const body = document.querySelector('body');
+    e.preventDefault();
+    e.stopPropagation();
     body.classList.toggle('navbar-open');
+  };
+
+  const documentClicked = () => {
+    const body = document.querySelector('body');
+
+    if (body.classList.contains('navbar-open')) {
+      body.classList.remove('navbar-open');
+    }
   };
 
   const start = () => {
     const hamburger = document.querySelector('.hamburger');
 
+
+    document.addEventListener('click', documentClicked);
     hamburger.addEventListener('click', hamburgerClicked);
     window.removeEventListener('DOMContentLoaded', start);
   };
